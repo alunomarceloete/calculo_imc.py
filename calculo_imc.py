8817f53dddfed8cfc0b6d80cd6488a29bcc306cc
Marcelo <alunomarcelo.ete@gmail.com>
2025-04-01 11:32:55 -0300

    Add files via upload

---
+++ calculo_imc.py
@@ -0,0 +1,8 @@
+
+#( CALCULO DE IMC ), ALUNO: MARCELO 
+nome = input('Digite o seu nome: ')
+peso = float(input('Informe seu peso: '))
+altu = float(input('Informe sua altura: '))
+calc = peso / altu**2
+print('Seu IMC é: ', calc)
+
